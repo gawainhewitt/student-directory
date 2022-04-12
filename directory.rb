ac482c88e97ab7960b3af5e6de6fe3571f610d84
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-04-12 22:38:07 +0100

    refactored menus

--- directory.rb
+++ directory.rb
@@ -1,9 +1,11 @@
+@students = []
+
 def input_students
   cohorts = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # create an empty array
-  students = []
+  @students = []
   # get the first name
   name = gets.strip
   # while the name is not empty, repeat this code
@@ -13,6 +15,7 @@ def input_students
     if hobby.empty? 
       hobby = "none given"
     end
+    hobby.downcase!
     puts "what is your cohort?"
     cohort = gets.chomp
     cohort_check = false
@@ -34,17 +37,17 @@ def input_students
       cohort = gets.chomp
     end
     # add the student hash to the array
-    students << {name: name, hobby: hobby, cohort: cohort.to_sym}
-    statement = "Now we have #{students.count} student"
-    if students.length > 1
+    @students << {name: name, hobby: hobby, cohort: cohort.to_sym}
+    statement = "Now we have #{@students.count} student"
+    if @students.length > 1
       statement << "s"
     end
     puts statement
     # get another name from the user
     name = gets.chomp
   end
-  # return the array of students
-  students
+  # return the array of @students
+  @students
 end
 
 def print_header
@@ -52,18 +55,18 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  if students.length != 0
+def print_students_list
+  if @students.length != 0
     idx = 0
     existing_cohorts = {}
     while true
-      if idx < students.length
-        if existing_cohorts[students[idx][:cohort]] == nil
-          existing_cohorts[students[idx][:cohort]] = []
+      if idx < @students.length
+        if existing_cohorts[@students[idx][:cohort]] == nil
+          existing_cohorts[@students[idx][:cohort]] = []
         end
-        existing_cohorts[students[idx][:cohort]].push({name: students[idx][:name], hobby: students[idx][:hobby]})
+        existing_cohorts[@students[idx][:cohort]].push({name: @students[idx][:name], hobby: @students[idx][:hobby]})
         #puts existing_cohorts
-        #puts "#{idx+1}. #{students[idx][:name]}, whos hobby is #{students[idx][:hobby]} (#{students[idx][:cohort]} cohort)".center(100)      
+        #puts "#{idx+1}. #{@students[idx][:name]}, whos hobby is #{@students[idx][:hobby]} (#{@students[idx][:cohort]} cohort)".center(100)      
         idx += 1
       else
         break
@@ -79,39 +82,47 @@ def print(students)
   end
 end
 
-def print_footer(names)
-  footer_text = "Overall, we have #{names.count} great student"
-  if names.length > 1
+def print_footer
+  footer_text = "Overall, we have #{@students.count} great student"
+  if @students.length > 1
     footer_text << "s"
-  elsif names.length == 0
+  elsif @students.length == 0
     footer_text = "We do not have any students!"
   end
   puts footer_text
 end
 
 def interactive_menu
-  students = []
-  loop do     # 4. repeat from step 1
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
+  loop do    
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
     when "1"
       students = input_students
     when "2"
-      print_header
-      print(students)
-      print_footer(students)
+      show_students
     when "9"
-      exit #this will cause the program to terminate
+      exit
     else
       puts "I don't know what you meant, try again"
-    end
   end
 end
+
 
 interactive_menu
